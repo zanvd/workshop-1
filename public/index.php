a74ffef090b7f3e745f7a472fa89dbee5df2b273
Jan Mrhar <jan.mrhar@preskok.si>
2018-04-06 09:33:54 +0200

    initial commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Make all paths relative to project root dir
+ */
+chdir(dirname(__DIR__));
+
+
+require 'vendor/autoload.php';
---
+++ src/Application/Application.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Workshop\Application;
+
+
+class Application
+{
+
+  /**
+   * @var Application $app
+   */
+  private $app = null;
+
+
+  /**
+   * Initializes framework
+   * @param string $configPath
+   */
+  public static function init(string $configPath)
+  {
+
+  }
+
+
+  /**
+   * Runs framework
+   */
+  public function run()
+  {
+
+  }
+
+
+}
